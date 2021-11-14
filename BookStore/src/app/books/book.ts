8da40f4ab8e6b61ec1f04253421fa5fb105e49c0
Andercou Alexandru <sandu.andercou@gmail.com>
2021-11-14 16:44:50 +0200

    commit login with hashing

---
+++ BookStore/src/app/books/book.ts
@@ -0,0 +1,13 @@
+export interface Book {
+
+    author: string,
+    country: string,
+    imageLink: string,
+    language: string,
+    link: string,
+    pages: number,
+    title: string,
+    year: number
+  
+   
+  }
--- BookStore/src/app/login/login.component.ts
+++ BookStore/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { HttpClientModule,HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { sha256, sha224 } from 'js-sha256';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +14,56 @@ export class LoginComponent implements OnInit {
 
   loginMethod(Item:any)
 {
- console.warn(Item);
- this.http.post(this.URL,Item);
+   console.log(Item["Password"]);
+   let hashPass=sha256(Item["Password"]);
+   console.log(hashPass);
+
+
+
+
+ //this.http.post(this.URL);
 }
+facebookLogIn()
+{
+
+console.log("facebbok");
+var script = document.createElement('script');
+script.src="https://connect.facebook.net/ro_RO/sdk.js#xfbml=1&version=v12.0&appId=3220700794679869&autoLogAppEvents=1" ;
+script.crossOrigin='anonymous';
+script.async=true;
+script.defer=true;
+script.nonce="ZzlS31zN";
+document.body.append(script);
+//script.textContent=" FB.login(function(response){});";
+
+script.onload=function() {
+   
+ 
+    alert("hello ba");
+
+
+};
+
+
+
+
+
+
+}
+twitterLogIn(){
+
+  console.log("twitter");
+
+}
+
+GmailLogIn()
+{
+
+  console.log("gmail");
+
+}
+
+
 
 
 
